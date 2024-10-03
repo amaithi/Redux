f69dd33c4a18aed012227299d926448e1f307333
Amaithi Singam J <amaithi.singam@bsigroup.com>
2024-10-03 12:28:51 +0530

    Updates

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import customerReducer from './slicers/customerSlicer'
-export const store = configureStore({
-    reducer:{
-        customer:customerReducer
-    }
-})
